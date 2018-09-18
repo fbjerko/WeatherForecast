cdc4919874ca563b9b612ea6597ea90411f2d0d3
fbjerko <f.bjerko@gmail.com>
2018-09-18 16:39:10 +0200

    Added displayMap.java

--- src/displayMap.java
+++ src/displayMap.java
@@ -4,5 +4,25 @@ import javax.servlet.http.*;
 
 public class displayMap extends HttpServlet {
 
+    PrintWriter out;
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+
+        // Set the response MIME type of the response message
+        response.setContentType("text/html");
+        // Allocate a output writer to write the response message into the network socket
+        out = response.getWriter();
+
+        // Write the response message, in an HTML page
+        try {
+            out.println("<html>\n<head>\n\t<title>Game Of Thrones Characters</title>\n</head>\n<body>");
+            
+            out.println("</body>\n</html>");
+        } finally {
+            out.close();  // Always close the output writer
+        }
+    }
 
 }
