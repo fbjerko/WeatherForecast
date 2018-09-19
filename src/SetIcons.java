e3547a488f28fa6f145cc136bb7a06765ae092aa
fbjerko <35508163+fbjerko@users.noreply.github.com>
2018-09-19 22:34:06 +0200

    Tighta opp

--- src/SetIcons.java
+++ src/SetIcons.java
@@ -5,23 +5,6 @@ import java.util.ArrayList;
 
 public class SetIcons {
 
-
-    /* Her må vi få sende get request til vær-api for å avgjøre
-    vær-ikon for byene i norge vi skal displaye
-
-    Typ:
-    - Oslo
-    - Bergen
-    - Trondheim
-    - Stavanger
-    - Tromsø
-    - Skedsmokorset **VIKTIG**
-    - Re kommune    **VIKTIG*
-
-
-    DisplayMap kaller på metoder herfra
-     */
-    // lat=60.10&lon=9.58
    static String oslo = "lat=59.91&lon=10.75";
    static String bergen = "lat=60.39&lon=05.32";
    static String trondheim = "lat=63.43&lon=10.39";
@@ -30,8 +13,6 @@ public class SetIcons {
    static String tromsø = "lat=69.64&lon=18.95";
 
     static JSONObject today, tomorrow;
-    static ArrayList<String> info;
-
 
     public static void main(String[] args) {
 
@@ -41,9 +22,7 @@ public class SetIcons {
         getXML(stavanger);
         getXML(kristiansand);
         getXML(tromsø);
-
     }
-
 
     public static void getXML(String longlat){
 
@@ -59,26 +38,14 @@ public class SetIcons {
 
             BufferedReader buffer = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 
-            info = new ArrayList<>();
-
-            StringBuilder stringBuilder = new StringBuilder();
             StringBuilder stringBuilder2 = new StringBuilder();
             String line;
 
-
-
             while ((line = buffer.readLine()) != null) {
-                info.add(line);
                 stringBuilder2.append(line + "\n");
             }
-
-            for(int i = 0; i<6; i++) {
-                stringBuilder.append(info.get(25+i) + "\n");
-            }
-
-
+      
             today = XML.toJSONObject(stringBuilder2.toString());
-
             today = today.getJSONObject("weatherdata").getJSONObject("product"); // om 24 timer
 
             for(int i = 0; i <260 ; i++) {
@@ -87,8 +54,6 @@ public class SetIcons {
                 }
                 char a = today.getJSONArray("time").getJSONObject(i).getString("to").charAt(11);
                 char b = today.getJSONArray("time").getJSONObject(i).getString("to").charAt(12);
-
-
 
                 if (a == '1' && b == '2') {
 
@@ -109,35 +74,11 @@ public class SetIcons {
                     System.out.println();
                     break;
                 }
-
             }
-
-
-
-           // System.out.println(today.getString("from") + " to " + tomorrow.getString("to"));
-           // System.out.println(tomorrow.getJSONObject("location").getJSONObject("symbol").getString("id"));
-
-
-            //System.out.println(object.getJSONObject("location").getJSONObject("symbol").getString("id"));
-            //System.out.println(object.getJSONObject("weatherdata").getJSONObject("product").getJSONArray("time"));
-
-
-            //System.out.println(object.getJSONArray("weatherdata"));
-           // System.out.println("1");
-           // System.out.println(object.getString("weatherdata"));
-           // System.out.println(object.getJSONArray("time").get);
-
-
         } catch(IOException e){
             System.out.println(e.toString());
         } catch(JSONException e){
             System.out.println(e.toString());
         }
-
     }
-
-
-    //Metoder
-
-
 }
