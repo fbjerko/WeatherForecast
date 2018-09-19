d6c983d212a78d752d822eaac5c1a1c58ff8cc76
fbjerko <f.bjerko@gmail.com>
2018-09-19 11:03:40 +0200

    Added SetIcons.java.

--- src/SetIcons.java
+++ src/SetIcons.java
@@ -1,3 +1,7 @@
+import org.json.*;
+import java.net.*;
+import java.io.*;
+
 public class SetIcons {
 
 
@@ -17,8 +21,48 @@ public class SetIcons {
     DisplayMap kaller på metoder herfra
      */
 
+    String Oslo = "";
+    String Bergen = "";
+    String Trondheim = "";
+    String Stavanger = "";
+    String Kristiansand = "";
+    String Tromsø = "";
+
+    JSONObject object;
+
+
     public static void main(String[] args) {
         //Bruker denne til testing
+    }
+
+
+    public JSONObject getCharacter(int id){
+        try {
+            URLConnection connection = new URL("https://anapioficeandfire.com/api/characters/" + id).openConnection();
+            connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+            connection.connect();
+
+            BufferedReader buffer = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+
+            StringBuilder stringBuilder = new StringBuilder();
+            String line;
+            while ((line = buffer.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+
+            object = new JSONObject(stringBuilder.toString());
+            System.out.println("</br>Name: "+ object.getString("name") + "</br>Gender: " + object.getString("gender")
+                    + "</br>Born: " + object.getString("born") + "</br>Main alias: " + object.getJSONArray("aliases").getString(0) + "</br>Played by: " + object.getJSONArray("playedBy").getString(0));
+
+
+        } catch(IOException e){
+            System.out.println(e.toString());
+        } catch(JSONException e){
+            System.out.println(e.toString());
+        }
+
+        return object;
     }
 
 
