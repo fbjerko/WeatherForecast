87610c6540807edfd95b951c84879b695e08b22c
fbjerko <f.bjerko@gmail.com>
2018-09-19 15:37:23 +0200

    Added SetIcons.java.

--- src/SetIcons.java
+++ src/SetIcons.java
@@ -21,24 +21,31 @@ public class SetIcons {
 
     DisplayMap kaller på metoder herfra
      */
-
-    String Oslo = "";
-    String Bergen = "";
-    String Trondheim = "";
-    String Stavanger = "";
-    String Kristiansand = "";
-    String Tromsø = "";
+    // lat=60.10&lon=9.58
+   static String oslo = "lat=59.91&lon=10.75";
+   static String bergen = "lat=60.39&lon=05.32";
+   static String trondheim = "lat=63.43&lon=10.39";
+   static String stavanger = "lat=58.97&lon=05.73";
+   static String kristiansand = "lat=58.15&lon=08.01";
+   static String tromsø = "lat=69.64&lon=18.95";
 
     static JSONObject today, tomorrow;
     static ArrayList<String> info;
 
 
     public static void main(String[] args) {
-        getXML();
+
+        getXML(oslo);
+        getXML(bergen);
+        getXML(trondheim);
+        getXML(stavanger);
+        getXML(kristiansand);
+        getXML(tromsø);
+
     }
 
 
-    public static void getXML(){
+    public static void getXML(String longlat){
 
         String timeToday;
         String symbolToday;
@@ -46,7 +53,7 @@ public class SetIcons {
         String timeNextDay;
         String symbolNextDay;
         try {
-            URLConnection connection = new URL("https://api.met.no/weatherapi/locationforecast/1.9/?lat=60.10&lon=9.58&msl=70").openConnection();
+            URLConnection connection = new URL("https://api.met.no/weatherapi/locationforecast/1.9/?" + longlat).openConnection();
             connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
             connection.connect();
 
@@ -69,37 +76,19 @@ public class SetIcons {
                 stringBuilder.append(info.get(25+i) + "\n");
             }
 
-            //System.out.println(stringBuilder2.toString());
-            //System.out.println(stringBuilder2.toString());
 
-            //System.out.println(stringBuilder.toString());
-           // object = new JSONArray(stringBuilder.toString());
-
-            tomorrow = today = XML.toJSONObject(stringBuilder2.toString());
-
-           // today = today.getJSONObject("weatherdata").getJSONObject("product").getJSONArray("time").getJSONObject(6); // om 24 timer
-
-            //tomorrow = tomorrow.getJSONObject("weatherdata").getJSONObject("product").getJSONArray("time").getJSONObject(130); // om 24 timer
+            today = XML.toJSONObject(stringBuilder2.toString());
 
             today = today.getJSONObject("weatherdata").getJSONObject("product"); // om 24 timer
 
-            tomorrow = tomorrow.getJSONObject("weatherdata").getJSONObject("product"); // om 24 timer
-
-
-          //  System.out.println(today.getString("from") + " to " + today.getString("to"));
-           // System.out.println(today.getJSONObject("location").getJSONObject("symbol").getString("id"));
-
-            System.out.println();
-
-
             for(int i = 0; i <260 ; i++) {
-
                 if(i % 5 == 0) {
                     continue;
                 }
-
                 char a = today.getJSONArray("time").getJSONObject(i).getString("to").charAt(11);
                 char b = today.getJSONArray("time").getJSONObject(i).getString("to").charAt(12);
+
+
 
                 if (a == '1' && b == '2') {
 
@@ -108,8 +97,19 @@ public class SetIcons {
                     System.out.println("Today : " + timeToday);
                     System.out.println("Weather : " + symbolToday);
 
+                    i += 120;
+                }
+
+                if (a == '1' && b == '2') {
+
+                    timeToday = today.getJSONArray("time").getJSONObject(i).getString("to");
+                    symbolToday = today.getJSONArray("time").getJSONObject(i).getJSONObject("location").getJSONObject("symbol").getString("id");
+                    System.out.println("Tomorrow : " + timeToday);
+                    System.out.println("Weather : " + symbolToday);
+                    System.out.println();
                     break;
                 }
+
             }
 
 
